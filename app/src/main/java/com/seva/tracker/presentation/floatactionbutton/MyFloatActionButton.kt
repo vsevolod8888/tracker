c18c0811940546b45e3ca74bd3fc4eb9c62c6642
vsevolod8888 <vsevolod88888888@gmail.com>
2025-03-05 02:38:33 +0200

    commit

--- app/src/main/java/com/seva/tracker/presentation/floatactionbutton/MyFloatActionButton.kt
+++ app/src/main/java/com/seva/tracker/presentation/floatactionbutton/MyFloatActionButton.kt
@@ -1,14 +1,20 @@
 package com.seva.tracker.presentation.floatactionbutton
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.seva.tracker.R
 import com.seva.tracker.presentation.bottomnavigation.NavigationItem
 
 @Composable
@@ -18,10 +24,12 @@ fun MyFloatingActionButton(navController: NavHostController, onClickMyFloatingAc
 
     when (currentRoute?.destination?.route) {
         NavigationItem.RoutesSmallCalendar.route -> {
-            FloatingActionButton(onClick = {
+            FloatingActionButton(modifier = Modifier.size(60.dp), onClick = {
                 onClickMyFloatingActionButton()
-            }) {
-                Icon(Icons.Default.Add, contentDescription = "Карта")
+            },
+                containerColor = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.5f),
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer) {
+                Icon(Icons.Default.Add, contentDescription = stringResource(R.string.addroute),modifier = Modifier.size(40.dp))
             }
         }
     }
